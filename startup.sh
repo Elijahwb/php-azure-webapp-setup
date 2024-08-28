5b4fc0efb0d7ffeba35c761f13269810e972745f
Wavamuno Brandon Elijah <39736143+Elijahwb@users.noreply.github.com>
2024-08-28 12:11:36 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -17,7 +17,7 @@ docker-php-ext-install gd
 apt-get install -y supervisor 
 
 cp /home/laravel-worker.conf /etc/supervisor/conf.d/laravel-worker.conf
-cp /home/laravel-worker.conf /etc/supervisor/conf.d/laravel-scheduler.conf
+cp /home/laravel-scheduler.conf /etc/supervisor/conf.d/laravel-scheduler.conf
 
 # restart nginx
 service nginx restart
