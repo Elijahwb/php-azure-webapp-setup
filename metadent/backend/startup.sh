fa976b27f895a3a33153bf33f59cbe4caeba2187
Brandon Elijah <brandonelijah099@gmail.com>
2024-10-25 09:27:22 +0300

    Update: update the startup script and store the startup command for reference

--- metadent/backend/startup.sh
+++ metadent/backend/startup.sh
@@ -1,4 +1,24 @@
+echo "Starting Metadent Backend..."
+echo ""
+
+echo "Copying php.ini..."
 cp /home/php.ini /usr/local/etc/php/conf.d/php.ini
+echo ""
+
+echo "Copying default..."
+cp /home/default /etc/nginx/sites-enabled/default
+echo ""
 
 # Restart the php engine of the server
+echo "Restarting php-fpm..."
 pkill -o -USR2 php-fpm
+echo ""
+echo "php-fpm restarted"
+echo ""
+
+# Restart nginx
+echo "Restarting nginx..."
+service nginx restart
+echo ""
+echo "nginx restarted"
+echo ""
